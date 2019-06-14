96c63b695d072cdb5626f8d429f1e47ab99be300
Nunogami <nuno3434@yahoo.co.jp>
2019-06-14 04:49:06 +0000

    Finish editing-users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,11 +21,13 @@ module SessionsHelper
   
   # セッションと@current_userを破棄します
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
 
   # 現在ログイン中のユーザーがいる場合オブジェクトを返します。
+  # それ以外の場合はcookiesに対応するユーザーを返します。
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -37,9 +39,24 @@ module SessionsHelper
       end
     end
   end
+  
+  def current_user?(user)
+    user == current_user
+  end
 
   # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
   def logged_in?
     !current_user.nil?
+  end
+  
+  # 記憶しているURL(またはデフォルトURL)にリダイレクトします。
+  def redirect_back_or(default_url)
+    redirect_to(session[:forwarding_url] || default_url)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを記憶します。
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
